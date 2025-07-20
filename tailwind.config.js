57e7be448e3613bd596c37fb3adb79e2fc83eb17
omprakash24d <omprakash24d@gmail.com>
2025-07-20 14:58:57 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-import type {Config} from 'tailwindcss';
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -119,4 +118,4 @@ export default {
     },
   },
   plugins: [require('tailwindcss-animate')],
-} satisfies Config;
+};
